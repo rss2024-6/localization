093c9c5624ffce0e4ba340bfa8c1622d4bbd1cab
Aimee <aimee003@mit.edu>
2024-04-03 20:21:42 +0000

    modify motion_model.py

--- localization/motion_model.py
+++ localization/motion_model.py
@@ -12,22 +12,6 @@ class MotionModel:
 
         ####################################
             
-    def x_to_T(self, x):
-        """
-        Transform pose vector into transformation matrix
-
-        args:
-            x: A 3-vector [x y theta]
-
-        return:
-            T: 3x3 transformation matrix
-        """
-        th = x[2]
-        R = [np.cos(th), -np.sin(th), np.sin(th), np.cos(th)]
-        T = np.array([[R[0], R[1], x[0]],[R[2], R[3], x[1]],[0, 0, 1]])
-        return T
-
-
     def evaluate(self, particles, odometry):
         """
         Update the particles to reflect probable
@@ -49,7 +33,7 @@ class MotionModel:
 
         ####################################
         # TODO
-        e_x  = 0.02          ## last best: 0.02, 0.01, 0.001
+        e_x  = 0.015          ## last best: 0.02, 0.01, 0.001
         e_y  = 0.01
         e_th = 0.001
         noise = np.array([np.random.normal(odometry[0], e_x,  len(particles)),
@@ -57,11 +41,12 @@ class MotionModel:
                           np.random.normal(odometry[2], e_th, len(particles))])
         noise = noise.T
         for i in range(len(particles)):
-            T  = self.x_to_T(particles[i])
-            dT = self.x_to_T(noise[i])
+            x  = particles[i]
+            dx = noise[i]
+            T  = np.array([[np.cos( x[2]), -np.sin( x[2]),  x[0]],[np.sin( x[2]), np.cos( x[2]),  x[1]],[0, 0, 1]])
+            dT = np.array([[np.cos(dx[2]), -np.sin(dx[2]), dx[0]],[np.sin(dx[2]), np.cos(dx[2]), dx[1]],[0, 0, 1]])
             T_new = T @ dT
             particles[i] = [T_new[0][2], T_new[1][2], np.arctan2(T_new[1][0], T_new[0][0])]
-            # p[i] = [T[0][2], T[1][2], np.arccos(T[0][0])] # Check original particle values
         return particles
 
         ####################################
