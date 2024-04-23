824778e02af9515914bdcc0ace303fe0acb85262
TyTodd <swagginty@gmail.com>
2024-04-23 20:41:11 +0000

    optimized particle filter

--- localization/particle_filter 2.py
+++ localization/particle_filter 2.py
@@ -2,15 +2,10 @@ from localization.sensor_model import SensorModel
 from localization.motion_model import MotionModel
 
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import PoseWithCovarianceStamped, Quaternion, TransformStamped, PoseArray, Pose
+from geometry_msgs.msg import PoseWithCovarianceStamped, Quaternion, TransformStamped
 from tf_transformations import euler_from_quaternion, quaternion_from_euler
-from std_msgs.msg import Float32
 import tf2_ros
 from sklearn.cluster import DBSCAN
-
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Point
 
 import numpy as np
 
@@ -26,10 +21,7 @@ class ParticleFilter(Node):
 
         self.declare_parameter('particle_filter_frame', "default")
         self.particle_filter_frame = self.get_parameter('particle_filter_frame').get_parameter_value().string_value
-        # Params
         self.simulation = True
-        self.odom_noise = True
-        # Optimizations
 
         #  *Important Note #1:* It is critical for your particle
         #     filter to obtain the following topic names from the
@@ -45,11 +37,9 @@ class ParticleFilter(Node):
         scan_topic = self.get_parameter("scan_topic").get_parameter_value().string_value
         odom_topic = self.get_parameter("odom_topic").get_parameter_value().string_value
 
-        sensor_timer_period = 1/5
         self.laser_sub = self.create_subscription(LaserScan, scan_topic,
                                                   self.laser_callback,
                                                   1)
-        self.sensor_model_timer = self.create_timer(sensor_timer_period, self.resample_particles)
 
         self.odom_sub = self.create_subscription(Odometry, odom_topic,
                                                  self.odom_callback,
@@ -74,29 +64,9 @@ class ParticleFilter(Node):
 
         self.odom_pub = self.create_publisher(Odometry, "/pf/pose/odom", 1)
 
-        self.pose_pub = self.create_publisher(PoseArray, "/pose_debug", 1)
-        timer_period = 1/20
-        self.timer = self.create_timer(timer_period, self.publish_average_pose_arrow)
-
-        self.pose_pub_points = self.create_publisher(Marker, "/pose_debug_points", 1)
-        timer_period = 1/20
-        self.timer = self.create_timer(timer_period, self.publish_particles_points)
-
-        self.snail_trail_pub = self.create_publisher(Marker, "/snail_trail", 1)
-        timer_period = 1/2
-        self.timer = self.create_timer(timer_period, self.publish_snail_trail)
-
-        self.variance_pub = self.create_publisher(Float32, "/variance", 1)
-        timer_period = 1/2
-        self.timer = self.create_timer(timer_period, self.publish_variance)
-
-
-
-
         # Initialize the models
         self.motion_model = MotionModel(self)
         self.sensor_model = SensorModel(self)
-        self.observation = None
 
         self.get_logger().info("=============+READY+=============")
 
@@ -111,59 +81,38 @@ class ParticleFilter(Node):
         # and the particle_filter_frame.
 
         self.particles_len = 500
-        self.particles = np.zeros((self.particles_len, 3)) # TODO initialize particles array properly
+        self.particles = np.empty(self.particles_len, 3) # TODO initialize particles array properly
         self.initial_pose = PoseWithCovarianceStamped()
         self.previous_pose = None
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
 
-        self.avg_pose = (0.,0.,0.)
-        self.snail_trail = []
-
     # sets the initial pose from rviz
     def pose_callback(self, msg):
         self.initial_pose = msg
-        #self.previous_pose = msg
         quaternion = self.initial_pose.pose.pose.orientation
-        #self.get_logger().info(str(quaternion))
         theta = euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])[2]
         self.particles = np.random.normal([msg.pose.pose.position.x, msg.pose.pose.position.y, theta], 0.1, (self.particles_len, 3))
 
-        #self.get_logger().info("Pose set")
 
     def laser_callback(self, msg):
-        self.observation = msg.ranges
-    
-    def resample_particles(self):
-        if self.observation is not None:
-            probabilities = self.sensor_model.evaluate(self.particles, self.observation)
-            probabilities /= np.sum(probabilities)
-            #"squash" sensor_model output (raise to the power of 1/3)
-
-            # resample particles
-            indicies = np.random.choice(len(self.particles), size=len(self.particles), p=probabilities)
-            self.particles = np.array([self.particles[i] for i in indicies])
-            self.publish_average_pose()
-
+        observation = msg.ranges
+        probabilities = self.sensor_model.evaluate(self.particles, observation)
+        # resample particles
+        indicies = np.random.choice(len(self.particles), size=len(self.particles), p=probabilities)
+        self.particles = [self.particles[i] for i in indicies]
+        self.publish_average_pose()
 
 
     def odom_callback(self, msg):
         twist = msg.twist.twist 
+        prev_twist = self.previous_pose.twist.twist
 
         if self.previous_pose is not None:
-            prev_twist = self.previous_pose.twist.twist
-                
-            vx = (twist.linear.x + prev_twist.linear.x) / 2 
+            vx = (twist.linear.x + prev_twist.linear.x) / 2
             vy = (twist.linear.y + prev_twist.linear.y) / 2
-            omega = (twist.angular.z + prev_twist.angular.z) / 2
-
-            if self.odom_noise:
-                vx = np.random.normal(vx, .1, 1)[0]
-                vy = np.random.normal(vy, .1, 1)[0]
-                omega = np.random.normal(omega, .1, 1)[0]
-
+            omega = (twist.linear.z + prev_twist.linear.z) / 2
 
             dt = (msg.header.stamp.sec - self.previous_pose.header.stamp.sec) + (msg.header.stamp.nanosec - self.previous_pose.header.stamp.nanosec) * 1e-9
-            #print("dt", dt)
 
             dx = vx * dt
             dy = vy * dt
@@ -172,7 +121,6 @@ class ParticleFilter(Node):
             odom_data = [dx, dy, dTheta]
 
             self.particles = self.motion_model.evaluate(self.particles, odom_data)
-            self.previous_pose = msg
         else:
             self.previous_pose = msg
         
@@ -198,11 +146,10 @@ class ParticleFilter(Node):
         # avg_theta = self.circular_mean([p[2] for p in self.particles])
 
         avg_pose = self.get_avg_pose(self.particles)
-        self.avg_pose = avg_pose
         avg_x = avg_pose[0]
         avg_y = avg_pose[1]
-        # avg_theta = avg_pose[2]
-        avg_theta = self.circular_mean([p[2] for p in self.particles])
+        avg_theta = avg_pose[2]
+        # avg_theta = self.circular_mean([p[2] for p in self.particles])
 
         odom = Odometry()
         odom.header.stamp = self.get_clock().now().to_msg()
@@ -242,8 +189,6 @@ class ParticleFilter(Node):
     
     def get_avg_pose(self, particles): #Mode clustering
         dbscan = DBSCAN(eps=1, min_samples=10)
-        particles = np.array(particles)
-        #print(particles[:5])
         clusters = dbscan.fit_predict(particles[:, :2])  # Only x and y coordinates are considered for clustering
 
         # Finding the cluster with the highest number of particles
@@ -255,140 +200,11 @@ class ParticleFilter(Node):
         # Calculating the average pose (x, y, and circular mean for theta) of the largest cluster
         average_pose = np.mean(largest_cluster_particles[:, :2], axis=0)  # Mean x and y
         average_theta = self.circular_mean(largest_cluster_particles[:, 2])
-        
 
         return average_pose[0], average_pose[1], average_theta
 
-    def publish_particles_arrows(self):
-        if self.pose_pub.get_subscription_count() == 0:
-            return 
-        msg = PoseArray()
-        msg.header.frame_id = 'map'
-        msg.header.stamp = self.get_clock().now().to_msg()
 
-        poses = []
-        for particle in self.particles:
-            pose = Pose()
-            pose.position.x = particle[0]
-            pose.position.y = particle[1]
-            q = quaternion_from_euler(0, 0, particle[2])
-            quaternion = Quaternion()
-            quaternion.x = q[0]
-            quaternion.y = q[1]
-            quaternion.z = q[2]
-            quaternion.w = q[3]
-
-            pose.orientation = quaternion
-
-            poses.append(pose)
-
-        msg.poses = poses
-        self.pose_pub.publish(msg)
-
-    def publish_average_pose_arrow(self):
-        if self.pose_pub.get_subscription_count() == 0:
-            return 
-        msg = PoseArray()
-        msg.header.frame_id = 'map'
-        msg.header.stamp = self.get_clock().now().to_msg()
-
-        poses = []
-
-        particle = self.avg_pose
-        pose = Pose()
-        pose.position.x = particle[0]
-        pose.position.y = particle[1]
-        q = quaternion_from_euler(0, 0, particle[2])
-        quaternion = Quaternion()
-        quaternion.x = q[0]
-        quaternion.y = q[1]
-        quaternion.z = q[2]
-        quaternion.w = q[3]
-        pose.orientation = quaternion
-
-        poses.append(pose)
-
-        msg.poses = poses
-        self.pose_pub.publish(msg)
-
-    def publish_snail_trail(self):
-        """
-        publishes line showing trail the robot has taken
-        """
-        # Create Marker message
-        if self.snail_trail_pub.get_subscription_count() == 0:
-            return 
-        marker_msg = Marker()
-        marker_msg.header.frame_id = 'map'  # Set the frame ID
-        marker_msg.header.stamp = self.get_clock().now().to_msg()
-        marker_msg.type = Marker.POINTS
-        marker_msg.action = Marker.ADD
-        marker_msg.scale.x = 0.05  # Set the scale of the points
-        marker_msg.scale.y = 0.05
-        marker_msg.scale.z = 0.05
-        marker_msg.color.a = 1.0  # Set the alpha value (transparency)
-        marker_msg.color.b = 1.0  # Set the color to red
-
-        self.snail_trail.append((self.avg_pose[0], self.avg_pose[1]))
-
-        # Populate the points
-        for point in self.snail_trail:
-            p = Point()
-            p.x = point[0]
-            p.y = point[1]
-            p.z = 0.0  # Assuming z-coordinate is 0
-            marker_msg.points.append(p)
-
-        self.snail_trail_pub.publish(marker_msg)
-
-    def publish_particles_points(self):
-        """
-        publishes a point for each
-        """
-        if self.pose_pub_points.get_subscription_count() == 0:
-            return 
-        # Example list of x, y coordinates
-        #points = [(1.0, 2.0), (3.0, 4.0), (5.0, 6.0)]
-        points = self.particles[:, :2]
-
-        # Create Marker message
-        marker_msg = Marker()
-        marker_msg.header.frame_id = 'map'  # Set the frame ID
-        marker_msg.header.stamp = self.get_clock().now().to_msg()
-        marker_msg.type = Marker.POINTS
-        marker_msg.action = Marker.ADD
-        marker_msg.scale.x = 0.05  # Set the scale of the points
-        marker_msg.scale.y = 0.05
-        marker_msg.scale.z = 0.05
-        marker_msg.color.a = 1.0  # Set the alpha value (transparency)
-        marker_msg.color.r = 1.0  # Set the color to red
-
-        # Populate the points
-        for point in points:
-            p = Point()
-            p.x = point[0]
-            p.y = point[1]
-            p.z = 0.0  # Assuming z-coordinate is 0
-            marker_msg.points.append(p)
-
-        self.pose_pub_points.publish(marker_msg)
-    
-    def publish_variance(self):
-        if self.variance_pub.get_subscription_count() == 0:
-            return 
-        msg = Float32()
-        columns = [self.particles[:, i] for i in range(self.particles.shape[1])]
-        variance_sum = 0
-        for column in columns:
-            variance_sum += np.var(column)
-    
-        variance = variance_sum/3
-        msg.data = variance
-        self.variance_pub.publish(msg)
-
-
-
-
+        
 
 
 
